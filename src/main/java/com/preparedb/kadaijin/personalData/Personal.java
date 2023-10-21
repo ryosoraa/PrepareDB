bf4371725068f895642d168c03d89065dc93f501
Ryosora <125730177+ryosoraa@users.noreply.github.com>
2023-10-21 16:44:23 +0700

    Update Personal.java

--- src/main/java/com/preparedb/kadaijin/personalData/Personal.java
+++ src/main/java/com/preparedb/kadaijin/personalData/Personal.java
@@ -1,5 +1,24 @@
 package com.preparedb.kadaijin.personalData;
 
 public class Personal {
-    
+    public String [] data(String name) {
+        List<String> name = new ArrayList<>();
+        String [] country = [""];
+        String [] city = [""];
+        Integer age = random;
+    }
+
+    public void personalDB (Connection connection) {
+        String path = "country.json";
+
+        ObjectMapper objectMapper = new ObjectMapper;
+        File file = new File(path);
+
+        JsonNode data = objectMapper.readTree("file");
+
+        for (JsonNode one : data) {
+            String country = one.asText;
+            System.out.println(country);
+        }
+    }
 }
